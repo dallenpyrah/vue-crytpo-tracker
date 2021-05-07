e40ea5151eab51438bcccac9d5e09b842e59f944
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-07 15:04:41 -0600

    getting data back for coins

---
+++ src/components/CoinComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-8">
+    <div class="card">
+      <h2>
+        {{ coinProp.name }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoinComponent',
+  props: { coinProp: { type: Object, required: true } }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/MarketPage.vue
+++ src/pages/MarketPage.vue
@@ -1,10 +1,46 @@
 <template>
-  marketpage
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-4 text-center">
+        <h5>Search for coins</h5>
+      </div>
+    </div>
+    <div class="row justify-content-center mt-3">
+      <div class="col-4">
+        <div class="form-group">
+          <input type="text"
+                 name=""
+                 id=""
+                 class="form-control p-4"
+                 placeholder="Bitcoin, Ethereum, Doge"
+          >
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <CoinComponent v-for="coin in state.marketData" :key="coin.id" :coin-prop="coin">
+      </CoinComponent>
+    </div>
+  </div>
 </template>
 
 <script>
+import { computed, onMounted, reactive } from 'vue'
+import { marketsService } from '../services/MarketService'
+import { AppState } from '../AppState'
+import CoinComponent from '../components/CoinComponent.vue'
 export default {
-  name: 'Market'
+  name: 'Market',
+  components: { CoinComponent },
+  setup() {
+    const state = reactive({
+      marketData: computed(() => AppState.marketData)
+    })
+    onMounted(() => marketsService.getAllMarketCoins())
+    return {
+      state
+    }
+  }
 }
 </script>
 
